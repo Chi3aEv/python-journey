f9b6dfeaaba563b27d2ce2d1841cfa3df81888d0
Bandya <chitamanisawant@gmail.com>
2026-09-05 22:40:41 +0530

    Basics of Python

---
+++ basics/first.py
@@ -0,0 +1,13 @@
+#this is my first python program 
+print("Hello world!")
+print('This is my first python program')
+print("I am learning python programming")
+print("My name is Borz")
+
+print("hello \"Borz\"\n")
+print()
+print ("pc : c:\\Users\\Borz\\Desktop\\python")
+
+
+
+print("Your Learning Path :\n - Python Basics \n - Data Engineering \n - AI")
